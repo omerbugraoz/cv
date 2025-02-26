ac8191ba772a1e90eefd5de77b593b9c8cb867a2
omerbugraoz <oomerbugraoz@gmail.com>
2025-02-26 22:56:37 +0300

    initial commit

---
+++ data/me.ts
@@ -0,0 +1,175 @@
+export const ME = {
+  name: "Ömer Buğra ÖZ",
+  title: "Frontend Developer",
+  contact: [
+    // {
+    //   name: "website",
+    //   href: "",
+    // },
+    {
+      name: "email",
+      href: "mailto:oomerbugraoz@gmail.com",
+    },
+    {
+      name: "phone",
+      href: "tel:+905067907173",
+    },
+    {
+      name: "linkedin",
+      href: "https://www.linkedin.com/in/omerbugraoz/",
+    },
+    {
+      name: "github",
+      href: "https://github.com/omerbugraoz",
+    },
+  ],
+  personalInfo: {
+    dateOfBirth: "04/05/1994",
+    nationality: "Turkish",
+  },
+  summary:
+    "Experienced Frontend Developer with expertise in Vue, React, Next.js and Nuxt.js. Skilled in building dynamic, interactive web applications with a focus on modularity, maintainability, and user experience.",
+  workExperience: [
+    {
+      position: "Frontend Developer",
+      company: "Dogus Otomotiv / BREW",
+      location: "İstanbul, Türkiye",
+      period: "2023 – Present",
+      description:
+        "Second-hand vehicles buying and selling - Sensat.com - Web platform that relates buying and selling cars.",
+      responsibilities: [
+        "Developed a dynamic and interactive frontend using Vue.js and Nuxt.js",
+        "Leveraged Pinia for effective and sustainable state management in the application.",
+        "Employed Bitbucket for our development source control management and synchronized with Dogus Otomotiv's Azure accounts to deliver code to their team.",
+        "Used Storybook to develop, test, and document reusable components, making our codebase modular and easy to maintain",
+      ],
+      technologies: ["Vue.js", "Nuxt.js", "Pinia", "Bitbucket", "Storybook"],
+    },
+    {
+      position: "Frontend Developer",
+      company: "Maxitech / BREW",
+      location: "San Francisco, United States",
+      period: "2024 – 2024",
+      description:
+        "Finance Web App - For Lunafı, a fınancial suit tailored for self-employed professionals in the United States, we developed a desktop application to complement their existing mobile app. Lunafi empowers users to identify and monitor tax deductions, estimate tax liabilities and savings, manage invoices, and obtain insights into both business and personal finances. The platform leverages automated tasks and AI-driven recommendations to enhance fınancial management and decision-making.",
+      websiteUrl: "https://maxitech.com/",
+      responsibilities: [
+        "Developed a web application using React, Next.js.",
+        "Implemented TailwindCss for styling",
+        "Used Recoil for state management",
+        "Integrated Plaid for smooth fınancial transactions.",
+        "Utilized Stripe for managing subscriptions.",
+        "Utilized Vercel for deploying the project and implemented CI/CD processes.",
+        "Utilized Storybook for developing, testing, and documenting reusable component",
+      ],
+      technologies: [
+        "React",
+        "Next.js",
+        "TailwindCSS",
+        "Recoil",
+        "Plaid",
+        "Stripe",
+        "Vercel",
+        "Storybook",
+      ],
+    },
+    {
+      position: "Frontend Developer",
+      company: "Koc Sistem / BREW",
+      location: "İstanbul, Türkiye",
+      period: "2023 – 2024",
+      description: "Koç Sistem Web Site",
+      websiteUrl: "https://www.kocsistem.com.tr/",
+      responsibilities: [
+        "Developed a web application using React, Remix, and Typescript",
+        "Integrated GraphQL for efficient data fetching and manipulation",
+        "Implemented multilingual capabilities (Turkish and English)(Arabic is in progress) with i18n",
+        "Utilized Storybook for developing, testing, and documenting reusable components",
+      ],
+      technologies: [
+        "React",
+        "Remix",
+        "TypeScript",
+        "GraphQL",
+        "i18n",
+        "Storybook",
+      ],
+    },
+    {
+      position: "Frontend Developer",
+      company: "Dogus Otomotiv / BREW",
+      location: "İstanbul, Türkiye",
+      period: "2022 – 2023",
+      description:
+        "Second-hand vehicles buying and selling - DOD - Web platform that relates buying and selling cars.",
+      responsibilities: [
+        "Developed a dynamic and interactive frontend using Vue.js and Nuxt.js",
+        "Styled based on Ant Design",
+        "Used Storybook to develop, test, and document reusable components, making our codebase modular and easy to maintain",
+      ],
+      technologies: ["Vue.js", "Nuxt.js", "Ant Design", "Storybook"],
+    },
+    {
+      position: "Frontend Developer",
+      company: "Carrefour France / BREW",
+      location: "France",
+      period: "2021 – 2021",
+      description:
+        "20 Megatons - Web site that relates greenhouse gas reduction",
+      responsibilities: [
+        "Developed a dynamic web application using Vue.js and Nuxt.js",
+        "Employed Strapi as the headless CMS for content management, enabling dynamic data handling.",
+        "Implemented multilingual capabilities (French and English) with i18n",
+        "Integrated REST APIs to facilitate smooth communication between the frontend and backend",
+        "Utilized Storybook for developing, testing, and documenting reusable components",
+      ],
+      technologies: [
+        "Vue.js",
+        "Nuxt.js",
+        "Strapi CMS",
+        "i18n",
+        "REST API",
+        "Storybook",
+      ],
+    },
+  ],
+  education: [
+    {
+      institution: "İzmir Institute of Technology",
+      degree: "Computer Engineering",
+      period: "2013 – 2019",
+      location: "İzmir, Türkiye",
+      websiteUrl: "https://iyte.edu.tr/",
+    },
+  ],
+  skills: [
+    "HTML",
+    "CSS (Sass, Less, Styled Components, Tailwind)",
+    "JavaScript",
+    "TypeScript",
+    "React",
+    "React Native",
+    "Vue",
+    "Nuxt.js",
+    "Next.js",
+    "Remix.js",
+    "GraphQL",
+    "Git",
+    "Hasura",
+    "Rest",
+    "Node.js",
+    "Fastify",
+    "Zustand",
+    "DirectusCMS",
+  ],
+  languages: [
+    {
+      name: "Turkish",
+      level: "Native",
+    },
+    {
+      name: "English",
+      level: "B2",
+    },
+  ],
+};
