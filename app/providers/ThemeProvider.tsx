0fe74763b4f94623c2b54def601039602dd652fb
omerbugraoz <oomerbugraoz@gmail.com>
2025-03-01 16:47:39 +0300

    chore: fix build error

--- app/providers/ThemeProvider.tsx
+++ app/providers/ThemeProvider.tsx
@@ -1,7 +1,15 @@
 "use client";
 
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import {
+  ThemeProvider as NextThemesProvider,
+  type ThemeProviderProps as NextThemeProviderProps,
+} from "next-themes";
+import { ReactNode } from "react";
 
-export function ThemeProvider({ children, ...props }: any) {
+type ThemeProviderProps = NextThemeProviderProps & {
+  children: ReactNode;
+};
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
